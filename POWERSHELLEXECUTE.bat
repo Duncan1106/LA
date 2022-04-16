cbbf137124274d8dabc2210c4c0f2846594cbae8
Duncan1106 <73903126+Duncan1106@users.noreply.github.com>
2022-04-16 12:24:34 +0200

    Update POWERSHELLEXECUTE.bat
    changes to get choice working

--- POWERSHELLEXECUTE.bat
+++ POWERSHELLEXECUTE.bat
@@ -1,28 +1,24 @@
 ## Batch File um die Powershell Scripte in eingescghränkten Umgebungen uneingeschränkt ausfüren zu können
+@echo off
+echo "Drücke 1 für automatisches Kopieren und umbenennen der Dateie, für die Erstellung "
+echo "eines neues Jahresverzeichnisses , 2 und 3 um das Programm zu beenden""
+CHOICE /N /C 123 /M "Wähle [1], [2] oder [3]"
 
-echo "Drücke A für automatisches Kopieren und umbenennen der Dateie, für die Erstellung eines neues Jahresverzeichnisses , B und Q um das Programm zu beenden"
-CHOICE /N /C:ABQ /M "Wähle A, B oder Q"%1
+IF %ERRORLEVEL% == 1 GOTO 1
+IF %ERRORLEVEL% == 2 GOTO 2
+IF %ERRORLEVEL% == 3 GOTO 3
 
-IF ERRORLEVEL ==1 GOTO A
-IF ERRORLEVEL ==2 GOTO B
-IF ERRORLEVEL ==3 GOTO Q
-
-A:
+:1
 echo "Ausfüren des Versand2History&RL Scriptes"
-PowerShell.exe -ExecutionPolicy Unrestricted -command "C:\Users\dunca\Desktop\versand2historyandRL.ps1" ## Verzeichnis muss angepaast werden
+PowerShell.exe -ExecutionPolicy Unrestricted -command "C:\Users\dunca\Desktop\versand2historyAndRL.ps1" ## Verzeichnis muss angepaast werden
 pause
 exit
 
-B:
+:2
 echo "Ausfüren des newHistoryYear Scriptes"
 PowerShell.exe -ExecutionPolicy Unrestricted -command "C:\Users\dunca\Desktop\newHistoryYear.ps1" ## Verzeichnis muss angepasst werden
 pause
 exit
 
-Q:
+:3
 exit
-
-
-
-
-
