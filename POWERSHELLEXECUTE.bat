c24d2a8361d680e94f8a456101b2d9522ea681f2
Duncan1106 <73903126+Duncan1106@users.noreply.github.com>
2022-04-16 15:52:02 +0200

    Update POWERSHELLEXECUTE.bat
    give option to rechoose your choice

--- POWERSHELLEXECUTE.bat
+++ POWERSHELLEXECUTE.bat
@@ -1,8 +1,9 @@
-## Batch File um die Powershell Scripte in eingescghränkten Umgebungen uneingeschränkt ausfüren zu können
+## Batch File um die Powershell Scripte in eingescghränkten Umgebungen uneingeschränkt ausführen zu können
 @echo off
-echo "Drücke 1 für automatisches Kopieren und umbenennen der Dateie, für die Erstellung "
+echo "Druecke 1 für automatisches Kopieren und umbenennen der Dateie, für die Erstellung "
 echo "eines neues Jahresverzeichnisses , 2 und 3 um das Programm zu beenden""
-CHOICE /N /C 123 /M "Wähle [1], [2] oder [3]"
+:0
+CHOICE /N /C 123 /M "Waehle [1], [2] oder [3]"
 
 IF %ERRORLEVEL% == 1 GOTO 1
 IF %ERRORLEVEL% == 2 GOTO 2
@@ -10,15 +11,21 @@ IF %ERRORLEVEL% == 3 GOTO 3
 
 :1
 echo "Ausfüren des Versand2History&RL Scriptes"
-PowerShell.exe -ExecutionPolicy Unrestricted -command "C:\Users\dunca\Desktop\versand2historyAndRL.ps1" ## Verzeichnis muss angepaast werden
+PowerShell.exe -ExecutionPolicy Unrestricted -command "./versand2historyAndRL.ps1" ## Verzeichnis muss angepaast werden
+GOTO end
 pause
 exit
 
 :2
 echo "Ausfüren des newHistoryYear Scriptes"
-PowerShell.exe -ExecutionPolicy Unrestricted -command "C:\Users\dunca\Desktop\newHistoryYear.ps1" ## Verzeichnis muss angepasst werden
-pause
-exit
+PowerShell.exe -ExecutionPolicy Unrestricted -command "./newHistoryYear.ps1" ## Verzeichnis muss angepasst werden
+GOTO end
+
 
 :3
 exit
+
+:end
+CHOICE /N /C YN /M "Programm beenden? [Y] zum beenden [N] zur Auswahl zurück."
+IF %ERRORLEVEL% == 1 GOTO 3
+IF %ERRORLEVEL% == 2 GOTO 0
