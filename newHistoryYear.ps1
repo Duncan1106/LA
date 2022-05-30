d391c1758ecdc158dcf2988e6c6ab70b910c605c
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-05-30 14:56:01 +0200

    Update newHistoryYear.ps1
    improved user input (year)
    improved the detection of a valid user input
    integrated new cli gui

--- newHistoryYear.ps1
+++ newHistoryYear.ps1
@@ -1,37 +1,41 @@
-## Verzeichnis in dem die ganzen alten Dateien liegen sollen
-$Directory = "./LA\history"
+## Verzeichnis in dem die ganzen alten Dateien liegen 
+    $Directory = ".\history"
+    # Name und Pfad der GUI
+    $gui = $args[1]
 
-## User-Input um das Jahr herauszufinden, für das das Verzeichnis erstellt werden soll
-$year = Read-Host -Prompt 'Input the year to create a new history directory for this year'
+## User-Input um das Jahr herauszufinden, fuer das das Verzeichnis erstellt werden soll
+    $year = [String]$args[0]
+    $dYear= (Get-Date).AddMonths(-1).ToString("yyyy")
+    $dyear2 = [int]$dYear + 1 
+    write-Host $year
 
-##User-Input überprüfung
-$dYear= (Get-Date).AddMonths(-1).ToString("yyyy")
-
-# Überprüfen, ob das angegebene Jahr kleiner als oder glecih dem aktuellen Jahr ist
-if ($year -le $dYear) {
-    echo "please enter a year that is in the future, the present and the past years allready exist"
-    pause
-    exit
-}
-
-## Überprüfen, ob das angebenen Jahr ein valides Jahr ist, heißt gleich 4 Zeichen lang ist
-if($year.Length -cne 4) {
-    echo "please enter a valid year with is not $year, example is: 2021, 2022, ..."
-    pause
-    exit
-}
+##Benutzer Eingaben Ueberpruefung
+    # Ueberpruefen, ob das angegebene Jahr kleiner als oder gleich dem aktuellen Jahr ist
+    if ($year -lt $dYear) {
+        write-Host "please enter a year that is in the future or the present, the past years should allready exist"
+        powershell.exe -ExecutionPolicy Unrestricted -command $gui
+    }
+    ## Ueberpruefen, ob das angebenen Jahr ein valides Jahr ist, heisst gleich 4 Zeichen lang ist
+    if($year.Length -cne 4) {
+        write-Host "please enter a valid year which is not $year, example is: $dyear, $dyear2, ..."
+        powershell.exe -ExecutionPolicy Unrestricted -command $gui
+    }
+    
 
 ## Wenn die Usereingabe korrekt war, erstellen des Jahresverzeichnisses, mit den Monaten als Unterordner, von 01 bis 12 durchnummeriert
-else {
-    New-Item -Path $Directory -Name $year -ItemType "directory"
-    $subdir = "$Directory\$year"
-    For ($i=1; $i -lt 13; $i++) {
-        if($i -lt 10) {
-            $zero2nine = "0$i"
-            New-Item -Path $subdir -Name $zero2nine -ItemType "directory"
+    else {
+        New-Item -Path $Directory -Name $year -ItemType "directory"
+        $subdir = "$Directory\$year"
+        For ($i=1; $i -lt 13; $i++) {
+            if($i -lt 10) {
+                $zero2nine = "0$i"
+                New-Item -Path $subdir -Name $zero2nine -ItemType "directory"
+            }
+            else{    
+                New-Item -Path $subdir -Name $i -ItemType "directory"
+            }
         }
-        else{    
-            New-Item -Path $subdir -Name $i -ItemType "directory"
-        }
+        powershell.exe -ExecutionPolicy Unrestricted -command $gui
+        write-host "History Script executed!"
     }
-}
+exit
